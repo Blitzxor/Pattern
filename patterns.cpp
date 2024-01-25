19f2dc497af826f92ed1455b2f7e10aa79d66bc1
Brhys9 <rhysget0@gmail.com>
2024-01-25 19:34:42 +0530

    +3

--- patterns.cpp
+++ patterns.cpp
@@ -50,13 +50,38 @@ void print6(int n){
         cout<<endl;
     }
 }
+void print7(int n){
+    for (int i =1;i<=n;i++){
+        for (int j =0;j<n-i+1;j++){
+            cout<<"#";
+        }
+        cout<<endl;
+    }
+}
+void print8(int n){
+    for (int i =1;i<=n;i++){
+        for (int j =0;j<=i;j++){
+            cout<<i;
+        }
+        cout<<endl;
+    }
+}
+void print9(int n){
+    for (int i =1;i<=n;i++){
+        for (int j =0;j<n-i+1;j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
+
 int main() {
     int t,n;
     cin>>t;
     for (int i =0;i<t;i++){
         int n;
         cin>>n;
-        print6(n);
+        print10(n);
     }
     return 0;
 
