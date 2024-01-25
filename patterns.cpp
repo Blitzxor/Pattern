2fbe973e6ef1cf818a991863c86c5ca95741df32
Brhys9 <rhysget0@gmail.com>
2024-01-25 19:03:36 +0530

    Basics skeleton for patterns

--- patterns.cpp
+++ patterns.cpp
@@ -1,8 +1,23 @@
-// Your First C++ Program
-
 #include <iostream>
-
+#include <vector>
+#include <algorithm>
+using namespace std;
+void print1(int n){
+    for (int i =0;i<n;i++){
+        for (int j =0;j<n;j++){
+            cout<<"#";
+        }
+        cout<<endl;
+    }
+}
 int main() {
-    std::cout << "Hello HHH!";
+    int t,n;
+    cin>>t;
+    for (int i =0;i<t;i++){
+        int n;
+        cin>>n;
+        print1(n);
+    }
     return 0;
+
 }
