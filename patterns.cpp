c796f6da458af9324f4625b442c130ff1836cbb9
Brhys9 <rhysget0@gmail.com>
2024-01-25 19:14:42 +0530

    BASICS 4 PATTERNS(last 2 more simplifoed)

--- patterns.cpp
+++ patterns.cpp
@@ -10,13 +10,53 @@ void print1(int n){
         cout<<endl;
     }
 }
+void print2(int n){
+    for (int i =0;i<n;i++){
+        for (int j =0;j<=i;j++){
+            cout<<"# ";
+        }
+        cout<<endl;
+    }
+}
+void print3(int n){
+    for (int i =0;i<n;i++){
+        for (int j =0;j<=i;j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
+void print4(int n){
+    for (int i =0;i<n;i++){
+        for (int j =0;j<=i;j++){
+            cout<<i;
+        }
+        cout<<endl;
+    }
+}
+void print5(int n){
+    for (int i =1;i<=n;i++){
+        for (int j =0;j<=i;j++){
+            cout<<i;
+        }
+        cout<<endl;
+    }
+}
+void print6(int n){
+    for (int i =1;i<=n;i++){
+        for (int j =0;j<i;j++){
+            cout<<i;
+        }
+        cout<<endl;
+    }
+}
 int main() {
     int t,n;
     cin>>t;
     for (int i =0;i<t;i++){
         int n;
         cin>>n;
-        print1(n);
+        print6(n);
     }
     return 0;
 
